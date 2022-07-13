479461de169bb9f73c384300ba20f7d25f9de325
masake <65312669+Namasake20@users.noreply.github.com>
2022-07-13 17:10:59 +0300

    Update HoroApi.kt

--- app/src/main/java/com/horoscopes/horoscope/data/HoroApi.kt
+++ app/src/main/java/com/horoscopes/horoscope/data/HoroApi.kt
@@ -12,7 +12,7 @@ interface HoroApi {
         const val BASE_URL = " https://sameer-kumar-aztro-v1.p.rapidapi.com"
     }
 
-    @Headers("X-RapidAPI-Host: sameer-kumar-aztro-v1.p.rapidapi.com","X-RapidAPI-Key: f012e35978msh30cdffe9f9c272bp160646jsn9f0062da586d")
+    @Headers()
     @POST(".")
     suspend fun getPredictions(@Query("sign") sign: String, @Query("day") day:String): Response<Forecast>
 }
